dca8ca9088c0bcdae067eda86fd0d77426ec06a6
siddharth singh <ss6156852@gmail.com>
2024-01-26 15:48:33 +0530

    fourth commit

--- basic/datatypes.js
+++ basic/datatypes.js
@@ -21,3 +21,14 @@ const myfcn=function(){
 
 console.log(typeof bignumber);
 console.log(typeof outsidetemp);// object type
+
+// stack (primitive), heap(non primitive)
+// stack me copy , heap me reference milta hai original value ka
+
+let oname="sid"
+let myname="siddhu"
+
+myname="siddharth"
+console.log(oname);
+console.log(myname);
+
