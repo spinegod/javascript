7c0238c08ec2655e4b415dbffef4619023ae54f4
siddharth singh <ss6156852@gmail.com>
2024-01-26 15:29:37 +0530

    third commit

---
+++ basic/datatypes.js
@@ -0,0 +1,23 @@
+//primitive
+// 7 types: String,Number,Boolean,null,undefined,Symbol,Bigint
+ 
+const bignumber=124652123225n
+const outsidetemp=null
+
+let check=true
+// reference(Non primitive)
+
+//Array,objects,fcn
+
+const heros=["donga","inspector steel","naagraaj"];
+let myobj={
+  name:"siddhu",
+  college: "mnnit",
+}
+
+const myfcn=function(){
+  console.log("Hello master");
+}
+
+console.log(typeof bignumber);
+console.log(typeof outsidetemp);// object type
