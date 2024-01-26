227bd387329b6fcf455b93fa1310090ab2dc781f
siddharth singh <ss6156852@gmail.com>
2024-01-26 14:52:30 +0530

    second commit

--- basic/conversions..js
+++ basic/conversions..js
@@ -8,8 +8,18 @@ let valueinnum=Number(score)//typecasting
 let isLoggedIn=1
 // converting 1 to its boolean
 let booleanisLoggedIn=Boolean(isLoggedIn)
-console.log(booleanisLoggedIn);
+//console.log(booleanisLoggedIn);
 // some conversion 
 // 1=>true 0=>false
 //""=>false
 // "sid"=>true
+// write more parenthesis when writing expression
+// equality(==) and comparisons(<,>,<=,>=) work differently in js
+// comparisons convert null to a number treating it as 0
+console.log(null>=0);//will give true
+console.log(null>0);//will give false
+console.log(null==0);//will give false
+// === strict checking ke liye use hota hai i.e same data type hone par hi check hoga
+//git remote add origin https://github.com/spinegod/javascript.git
+//git branch -M main
+//git push -u origin main
